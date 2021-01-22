6a0f55d7dc730857f6f7d005de0f7d346d06e943
Rommy Gustiawan <rgcppp@gmail.com>
2021-01-22 11:26:15 +0700

    Menghapus method authorize() -> requests class

--- app/Http/Requests/SignUpRequest.php
+++ app/Http/Requests/SignUpRequest.php
@@ -5,16 +5,6 @@ namespace App\Http\Requests;
 class SignUpRequest extends APIFormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
-
-    /**
      * Get the validation rules that apply to the request.
      *
      * @return array

bd6164c7cb345e0c570a237d0e1ee8661b82c042
Rommy Gustiawan <rgcppp@gmail.com>
2021-01-26 00:10:01 +0700

    Menghapus $casts attributes -> model user

--- app/Models/User.php
+++ app/Models/User.php
@@ -37,15 +37,6 @@ class User extends Authenticatable
         'remember_token'
     ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime'
-    ];
-
     protected $append = [
         'token',
         'total_uncompleted_assignment',

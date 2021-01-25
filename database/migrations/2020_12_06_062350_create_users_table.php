28bb67f2a7600e8c2baff22787c69548f0dfa05f
Rommy Gustiawan <rgcppp@gmail.com>
2021-01-25 23:30:59 +0700

    Menghapus field remember_token -> migrasi users

--- database/migrations/2020_12_06_062350_create_users_table.php
+++ database/migrations/2020_12_06_062350_create_users_table.php
@@ -19,7 +19,6 @@ class CreateUsersTable extends Migration
             $table->string('email', 50)->unique()->index();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
